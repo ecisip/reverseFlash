c7e5828f15beb73b5443e7723c7fbf1eab469c3d
ecisip <emmersonisip07@gmail.com>
2023-10-14 17:26:14 +0800

    added skip and change def buttons

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const content = document.getElementById("contentwrap");
 let flashcards = {};
+let flashbin = {};
 let revtitle = "";
+
+
 
 
 
@@ -9,6 +12,8 @@ function start (topicText) {
         alert("Topic name can't be empty");
         return;
     }
+
+
 
     content.innerHTML = "";
     revtitle = topicText;
@@ -48,6 +53,8 @@ function start (topicText) {
     content.appendChild(document.createElement("br"));
     content.appendChild(flashPrev);
 
+
+
     function createFlash (flashName) {
         if (flashName.trim().length === 0) {
             alert("Reverse flash card can't be empty");
@@ -67,6 +74,8 @@ function start (topicText) {
         }
     }
 
+
+
     function done () {
         content.innerHTML = "";
 
@@ -80,6 +89,8 @@ function start (topicText) {
         content.appendChild(revButton);
     }
 }
+
+
 
 
 
@@ -99,27 +110,78 @@ function review () {
 
     generateFlash();
 
+
+
     function generateFlash() {
+        flashDiv.innerHTML = "";
+
         const randomizer = Math.floor(Math.random()*Object.keys(flashcards).length);
-        let flashcardPick = Object.keys(flashcards)[randomizer];
+        let cardName = Object.keys(flashcards)[randomizer];
+        let cardDef = Object.values(flashcards)[randomizer];
 
         const flashcardName = document.createElement("h1");
-        flashcardName.textContent = `${flashcardPick}`;
+        flashcardName.textContent = `${cardName}`;
         const flashcardDef = document.createElement("p");
-        flashcardDef.textContent = `Your definition: ${Object.values(flashcards)[randomizer]}`;
-        const googler = document.createElement("a");
-        googler.setAttribute("target", "_blank");
-        googler.href = `https://www.google.com/search?q=${Object.keys(flashcards)[randomizer]}`
-        googler.textContent = `Google "${Object.keys(flashcards)[randomizer]}"`;
-        const googler2 = document.createElement("a");
-        googler2.setAttribute("target", "_blank");
-        googler2.href = `https://www.google.com/search?q=${Object.keys(flashcards)[randomizer]}%20${revtitle}`
-        googler2.textContent = `Google "${Object.keys(flashcards)[randomizer]} ${revtitle}"`;
-
+        flashcardDef.textContent = `Your definition: ${cardDef}`;
         flashDiv.appendChild(flashcardName);
         flashDiv.appendChild(flashcardDef);
+
+        const changeButton = document.createElement("button");
+        changeButton.setAttribute("type", "button");
+        changeButton.textContent = "Change definition";
+        changeButton.onclick = () => change(cardName, cardDef, flashcardDef);
+        const skipButton = document.createElement("button");
+        skipButton.setAttribute("type", "button");
+        skipButton.textContent = "Skip";
+        skipButton.onclick = () => generateFlash();
+        const okButton = document.createElement("button");
+        okButton.setAttribute("type", "button");
+        okButton.textContent = "Satisfied? Press Done!";
+        okButton.disabled = (cardDef.trim().length === 0) ? true : false;
+        //okButton.onclick = () => ok();
+
+        flashDiv.appendChild(changeButton);
+        flashDiv.appendChild(skipButton);
+        flashDiv.appendChild(okButton);
+        flashDiv.appendChild(document.createElement("br"));
+
+        const googler = document.createElement("a");
+        googler.setAttribute("target", "_blank");
+        googler.href = `https://www.google.com/search?q=${cardName}`
+        googler.textContent = `Google "${cardName}"`;
+        const googler2 = document.createElement("a");
+        googler2.setAttribute("target", "_blank");
+        googler2.href = `https://www.google.com/search?q=${cardName}%20${revtitle}`
+        googler2.textContent = `Google "${cardName} ${revtitle}"`;
         flashDiv.appendChild(googler);
         flashDiv.appendChild(document.createElement("br"));
         flashDiv.appendChild(googler2);
+    }
+
+
+
+    function change (cardName, cardDef, flashcardDef) {
+        const window = document.createElement("div");
+        flashDiv.appendChild(window);
+
+        const defBoxLabel = document.createElement("p");
+        defBoxLabel.textContent = `Change definition:`;
+        const defBox = document.createElement("input");
+        defBox.setAttribute("type", "text");
+        defBox.value = cardDef;
+        const defBoxSubmit = document.createElement("button");
+        defBoxSubmit.setAttribute("type", "button");
+        defBoxSubmit.textContent = "Update definition";
+        defBoxSubmit.onclick = () => {
+            flashcards[cardName] = defBox.value;
+            window.remove();
+            flashcardDef.textContent = `${defBox.value}`;
+        };
+
+        window.appendChild(document.createElement("br"));
+        window.appendChild(defBoxLabel);
+        window.appendChild(defBox);
+        window.appendChild(defBoxSubmit);
+        window.appendChild(document.createElement("br"));
     }
 }
