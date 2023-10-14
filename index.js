efd45c7aec714d29c811b600c3ceebb885b1c5be
ecisip <emmersonisip07@gmail.com>
2023-10-14 14:58:24 +0800

    finished start screen

--- index.js
+++ index.js
@@ -19,11 +19,10 @@ function start () {
     flashNamerSubmit.setAttribute("type", "button");
     flashNamerSubmit.textContent = "Create reverse flash";
     flashNamerSubmit.onclick = () => createFlash(flashNamer.value);
-    const divider = document.createElement("br");
     
     content.appendChild(flashNamerLabel);
     content.appendChild(flashNamer);
-    content.appendChild(divider);
+    content.appendChild(document.createElement("br"));
     content.appendChild(flashNamerSubmit);
 
     const doneButton = document.createElement("button");
@@ -31,17 +30,29 @@ function start () {
     doneButton.setAttribute("disabled", true);
     doneButton.textContent = "Done!";
     doneButton.onclick = () => done();
-    content.appendChild(divider);
+    content.appendChild(document.createElement("br"));
+    content.appendChild(document.createElement("br"));
     content.appendChild(doneButton);
+
+    const flashPrev = document.createElement("ul");
+    content.appendChild(document.createElement("br"));
+    content.appendChild(document.createElement("br"));
+    content.appendChild(flashPrev);
 
     function createFlash (flashName) {
         if (flashName.trim().length === 0) {
             alert("Reverse flash card can't be empty");
+        } else if (flashName in flashcards) {
+            alert("Reverse flash already exists");
         } else {
             flashcards[flashName] = "";
             let flashcardnum = document.getElementById("flashcardnum");
             flashcardnum.textContent = `${Object.keys(flashcards).length}`;
             flashNamer.value = "";
+
+            const entryPrev = document.createElement("li");
+            entryPrev.textContent = flashName;
+            flashPrev.appendChild(entryPrev);
 
             doneButton.disabled = false;
         }
@@ -55,8 +66,12 @@ function start () {
         const revButton = document.createElement("button");
         revButton.setAttribute("type", "button");
         revButton.textContent = "Start reviewing!";
-        //revButton.onclick = () => review();
+        revButton.onclick = () => review();
         content.appendChild(doneTitle);
         content.appendChild(revButton);
     }
+}
+
+function review () {
+    content.innerHTML = "";
 }
